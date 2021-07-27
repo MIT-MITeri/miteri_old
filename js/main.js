888c097072bcecec5d395aad5ec96634d10a8799
kafleprabhakar <pkafleprabhakar@gmail.com>
2021-07-27 16:00:34 -0400

    events page and working collapse button

--- js/main.js
+++ js/main.js
@@ -16,24 +16,52 @@ const toggleNavClass = () => {
 
 window.addEventListener('scroll', () => toggleNavClass());
 
+function loadTeam() {
+  fetch('/content/team.json')
+    .then( response => response.json() )
+    .then( data => {
+      const members = data['members'];
+      const template = document.querySelector(".single-member.template");
+      const container = template.parentElement;
+      for (let member of members) {
+        const element = template.cloneNode(true);
+        element.classList.remove('template');
+
+        const img = element.querySelector('.member-photo');
+        img.setAttribute('src', '/img/'+member.image);
+        img.setAttribute('alt', member.name);
+
+        element.querySelector('.member-name').innerHTML = member.name;
+        element.querySelector('.member-title').innerHTML = member.title;
+        element.querySelector('.member-class').innerHTML = member.class;
+        element.querySelector('.member-course').innerHTML = member.course;
+        container.appendChild(element);
+      }
+    } );
+}
+
+function loadEvents() {
+  fetch('/content/events.json')
+    .then( response => response.json() )
+    .then( data => {
+      const events = data['events'];
+      const template = document.querySelector(".single-event.template");
+      const container = template.parentElement;
+      for (let event of events) {
+        const element = template.cloneNode(true);
+        element.classList.remove('template');
+
+        const img = element.querySelector('.event-photo');
+        img.setAttribute('src', '/img/'+event.photo);
+        img.setAttribute('alt', event.description);
+
+        element.querySelector('.event-description').innerHTML = event.description;
+        container.appendChild(element);
+      }
+    } );
+}
+
 window.addEventListener('load', () => {
-  fetch('/content/team.json').then( response => response.json() ).then( data => {
-    const members = data['members'];
-    const template = document.querySelector(".single-member.template");
-    const container = template.parentElement;
-    for (let member of members) {
-      const element = template.cloneNode(true);
-      element.classList.remove('template');
-
-      const img = element.querySelector('.member-photo');
-      img.setAttribute('src', '/img/'+member.image);
-      img.setAttribute('alt', member.name);
-
-      element.querySelector('.member-name').innerHTML = member.name;
-      element.querySelector('.member-title').innerHTML = member.title;
-      element.querySelector('.member-class').innerHTML = member.class;
-      element.querySelector('.member-course').innerHTML = member.course;
-      container.appendChild(element);
-    }
-  } )
+  loadTeam();
+  loadEvents();
 });
